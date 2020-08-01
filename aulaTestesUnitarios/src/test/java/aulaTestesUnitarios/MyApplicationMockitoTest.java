1dfb2a934ae181ecc465c591928f557987a7d191
edimailza <ebsilva5@stefanini.com>
2020-08-01 18:48:58 -0300

    [DECIMO PRIMEIRO COMMIT] Aula de Sonar

--- aulaTestesUnitarios/src/test/java/aulaTestesUnitarios/MyApplicationMockitoTest.java
+++ aulaTestesUnitarios/src/test/java/aulaTestesUnitarios/MyApplicationMockitoTest.java
@@ -1,13 +1,13 @@
 package aulaTestesUnitarios;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import aulaDeTestesUnitarios.Message;
@@ -26,13 +26,13 @@ public class MyApplicationMockitoTest {
 
 	@Test
 	public void processMessage_withMsgAndRec_true() {
-		when(service.send(anyString(), anyString())).thenReturn(true);
+		Mockito.when(service.send(null, null)).thenReturn(true);
 		Assertions.assertTrue(application.processMessage2(message));
 	}
 
 	@Test
 	public void processMessage_withMsgAndRec_false() {
-		when(service.send(anyString(), anyString())).thenReturn(false);
+		Mockito.when(service.send(null, null)).thenReturn(false);
 		Assertions.assertFalse(application.processMessage2(message));
 	}
 
